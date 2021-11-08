c373585e24278beb5735fd7b9884e0986f5df6a8
0xecho <0xecho@proto-code.com>
2021-11-08 15:28:13 +0300

    Make imporatble

--- app.py
+++ app.py
@@ -1,29 +1,33 @@
 import os, json, logging
+
 class DumbDB(object):
     """
-        DumbDB: A really stoopud database that just json dumps every value you give it :)
+        DumbDB: A really stoopid database that just json dumps every value you give it :)
 
     """
     
-    def __init__(self, location=None):
+    def __init__(self, location=None, file_name=None):
         if location == None:
-            location = "data.json"
-        self.location = os.path.expanduser(location)
-        self.load(self.location)
+            location = os.curdir
+        if file_name == None:
+            file_name = "data.json"
+        full_path = os.path.join(location, file_name)
+        self.full_path = os.path.expanduser(full_path)
+        self.load(self.full_path)
 
-    def load(self, location):
-       if os.path.exists(location):
+    def load(self, full_path):
+       if os.path.exists(full_path):
            self._load()
        else:
            self.data = {}
        return True
 
     def _load(self):
-        self.data = json.load(open(self.location, "r"))
+        self.data = json.load(open(self.full_path, "r"))
 
     def dumpdb(self):
         try:
-            json.dump(self.data, open(self.location, "w"))
+            json.dump(self.data, open(self.full_path, "w"))
             return True
         except:
             return False
@@ -33,14 +37,16 @@ class DumbDB(object):
             self.data[str(key)] = value
             self.dumpdb()
         except Exception as e:
-            logging.error("Couldn't save values to db", str(e))
+            logging.error("Couldn't save values to db")
+            logging.debug(str(e))
             return False
 
     def get(self, key, default=[]):
         try:
             return self.data[key]
         except KeyError as e:
-            logging.error("Couldn't fetch values from db", str(e))
+            logging.error("Couldn't fetch values from db")
+            logging.debug(str(e))
             return default
 
     def delete(self, key):
